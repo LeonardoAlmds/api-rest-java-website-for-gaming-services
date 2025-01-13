c54682395070c639c309961c3232ba13a3669603
LeonardoAlmds <leonardoalmd8@outlook.com>
2025-01-13 08:43:08 -0300

    first commit

---
+++ src/main/java/com/kplusweb/services_games/ServicesGamesApplication.java
@@ -0,0 +1,13 @@
+package com.kplusweb.services_games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesGamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kplusweb/services_games/ServicesGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kplusweb.services_games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
