875a0dc4d6ab1ff4d24811cb39b3310a648f6600
LeonardoAlmds <leonardoalmd8@outlook.com>
2025-01-13 12:14:42 -0300

    provisional security config

--- src/main/java/com/kplusweb/services_games/security/SecurityConfig.java
+++ src/main/java/com/kplusweb/services_games/security/SecurityConfig.java
@@ -1,7 +1,18 @@
 package com.kplusweb.services_games.security;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class SecurityConfig implements WebMvcConfigurer{
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.build();
+    }
 
 }
