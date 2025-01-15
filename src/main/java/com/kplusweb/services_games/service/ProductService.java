5972ccb10bea9164bd180eaa44e74df703bba56c
LeonardoAlmds <leonardoalmd8@outlook.com>
2025-01-15 09:30:35 -0300

    product productService refactor

--- src/main/java/com/kplusweb/services_games/service/ProductService.java
+++ src/main/java/com/kplusweb/services_games/service/ProductService.java
@@ -2,6 +2,7 @@ package com.kplusweb.services_games.service;
 
 import com.kplusweb.services_games.dtos.ProductDTO;
 import com.kplusweb.services_games.entity.Product;
+import com.kplusweb.services_games.entity.Category;
 import com.kplusweb.services_games.repositories.CategoryRepository;
 import com.kplusweb.services_games.repositories.ProductRepository;
 
@@ -13,34 +14,42 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
 
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, CategoryRepository categoryRepository) {
         this.productRepository = productRepository;
+        this.categoryRepository = categoryRepository;
     }
 
     public List<ProductDTO> getAllProducts() {
         List<Product> products = productRepository.findAll();
         return products.stream()
-                .map(product -> new ProductDTO(
-                        product.getId(),
-                        product.getName(),
-                        product.getDescription(),
-                        product.getImage_url(),
-                        product.getPrice(),
-                        product.getStock_quantity(),
-                        product.getSold_quantity(),
-                        product.getPosted_date(),
-                        product.getStatus().toString(),
-                        product.getRating(),
-                        product.getCategory()
-                ))
+                .map(this::convertToDTO)
                 .collect(Collectors.toList());
     }
 
     public ProductDTO getProductById(Long id) {
         Product product = productRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("product not found" + id));
+                .orElseThrow(() -> new RuntimeException("product not found " + id));
+        return convertToDTO(product);
+    }
 
+    public String postProduct(ProductDTO productDTO) {
+        Product product = convertToEntity(productDTO);
+        productRepository.save(product);
+        return "Product added successfully";
+    }
+
+    public String updateProduct(Long id, ProductDTO productDTO) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("product not found " + id));
+
+        updateEntity(product, productDTO);
+        productRepository.save(product);
+        return "Product updated successfully";
+    }
+
+    private ProductDTO convertToDTO(Product product) {
         return new ProductDTO(
                 product.getId(),
                 product.getName(),
@@ -56,28 +65,13 @@ public class ProductService {
         );
     }
 
-    public String postProduct(ProductDTO productDTO) {
+    private Product convertToEntity(ProductDTO productDTO) {
         Product product = new Product();
-        product.setName(productDTO.name());
-        product.setDescription(productDTO.description());
-        product.setImage_url(productDTO.image_url());
-        product.setPrice(productDTO.price());
-        product.setStock_quantity(productDTO.stock_quantity());
-        product.setSold_quantity(productDTO.sold_quantity());
-        product.setPosted_date(productDTO.posted_date());
-        product.setStatus(Product.Status.valueOf(productDTO.status()));
-        product.setRating(productDTO.rating());
-        product.setCategory(productDTO.category_id());
-
-        Product savedProduct = productRepository.save(product);
-        return "Product added successfully";
+        updateEntity(product, productDTO);
+        return product;
     }
 
-    public String updateProduct(Long id, ProductDTO productDTO) {
-        Product product = productRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("product not found: " + id));
-
-
+    private void updateEntity(Product product, ProductDTO productDTO) {
         product.setName(productDTO.name());
         product.setDescription(productDTO.description());
         product.setImage_url(productDTO.image_url());
@@ -88,8 +82,5 @@ public class ProductService {
         product.setStatus(Product.Status.valueOf(productDTO.status()));
         product.setRating(productDTO.rating());
         product.setCategory(productDTO.category_id());
-
-        productRepository.save(product);
-        return "Product updated successfully";
     }
 }
